d6b9bc59c515b2d9bc0d15b14c522a7317b1a380
Vivek Kumar <vk20225379@gmail.com>
2025-08-03 00:56:05 +0530

    Update server.js
    Remove unnecessary lines

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,18 +1,8 @@
 require('dotenv').config()
 const app = require('./src/app');
-// const path = require('path')
-// const express = require('express')
-
-
-// // connect static files of frontend
-// app.use(express.static(path.join(__dirname, './FrontEnd/dist')));
-
-// app.get("*", function (req,res) {
-//     res.sendFile(path.join(__dirname, "./FrontEnd/dist.index.html"));
-// })
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000')
 })
 
-// npx nodemon
+// to start server use this command `npx nodemon`
