9d13c5ed7264f34ef9adbc67202f5809e1e32147
Vivek Kumar <118986548+vivekxsh@users.noreply.github.com>
2025-08-02 16:45:29 +0530

    Final code
    Deployment

---
+++ BackEnd/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const app = require('./src/app');
+const path = require('path')
+const express = require('express')
+
+
+// connect static files of frontend
+app.use(express.static(path.join(__dirname, './FrontEnd/dist')));
+
+app.get("*", function (req,res) {
+    res.sendFile(path.join(__dirname, "./FrontEnd/dist.index.html"));
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000')
+})
+
+// npx nodemon
